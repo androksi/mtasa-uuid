4edf427c6429fdececc73073faa0cf70e3fcf8c9
Andrei Rossignolli <73851736+androksi@users.noreply.github.com>
2022-09-15 08:38:06 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,6 +1,6 @@
 local UUID_TEMPLATE_STRING = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 
-function string.uuid()
+function new()
     return string.gsub(UUID_TEMPLATE_STRING, "[x]", function()
         return string.format("%x", math.random(1, 15))
     end)
